1e0219203edb209102df60f9616a3e956f7d02e9
AlekseiKarakulov <146084376+AlekseiKar@users.noreply.github.com>
2024-04-03 22:04:48 +0300

    Update вторая задачка.py

--- вторая задачка.py
+++ вторая задачка.py
@@ -1,1 +1,13 @@
+import re
 
+def calculate_total_price(check):
+    total_price = 0
+    
+    pattern = r'([a-z]+)(\d+\.*\d*)'
+    matches = re.findall(pattern, check)
+    
+    for match in matches:
+        price = float(match[1])
+        total_price += price
+    
+    return total_price
