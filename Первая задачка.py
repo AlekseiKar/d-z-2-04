c5abbd5a391a4598f086467259285828c71419ca
AlekseiKarakulov <146084376+AlekseiKar@users.noreply.github.com>
2024-04-03 22:00:53 +0300

    Create Первая задачка.py

---
+++ Первая задачка.py
@@ -0,0 +1,20 @@
+def transform_number(a, b):
+    if a > b:
+        print("Невозможно")
+        return
+    
+    while a != b:
+        if b % 2 == 0 and b > a:
+            b //= 2
+        elif str(b)[-1] == '1' and b > a:
+            b = int(str(b)[:-1])
+        else:
+            print("Невозможно")
+            return
+    
+    print("из a можно получить b")
+
+# Пример использования:
+a = 3
+b = 45
+transform_number(a, b)
